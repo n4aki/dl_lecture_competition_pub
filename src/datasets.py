2d9dff7f8c19d2ad399d19bae9887075217174a1
aki <144330433+n4aki@users.noreply.github.com>
2024-07-18 15:35:29 +0900

    Update datasets.py

--- src/datasets.py
+++ src/datasets.py
@@ -4,10 +4,10 @@ import torch
 from typing import Tuple
 from termcolor import cprint
 from glob import glob
-
+from scipy.signal import butter, lfilter
 
 class ThingsMEGDataset(torch.utils.data.Dataset):
-    def __init__(self, split: str, data_dir: str = "data") -> None:
+    def __init__(self, split: str, data_dir: str = "data", preprocess: bool = False) -> None:
         super().__init__()
         assert split in ["train", "val", "test"], f"Invalid split: {split}"
         
@@ -15,25 +15,56 @@ class ThingsMEGDataset(torch.utils.data.Dataset):
         self.data_dir = data_dir
         self.num_classes = 1854
         self.num_samples = len(glob(os.path.join(data_dir, f"{split}_X", "*.npy")))
+        self.preprocess = preprocess
 
     def __len__(self) -> int:
         return self.num_samples
 
     def __getitem__(self, i):
         X_path = os.path.join(self.data_dir, f"{self.split}_X", str(i).zfill(5) + ".npy")
-        X = torch.from_numpy(np.load(X_path))
+        X = torch.from_numpy(np.load(X_path)).float()
+        
+        if self.preprocess:
+            X = self.preprocess_data(X)
         
         subject_idx_path = os.path.join(self.data_dir, f"{self.split}_subject_idxs", str(i).zfill(5) + ".npy")
         subject_idx = torch.from_numpy(np.load(subject_idx_path))
         
         if self.split in ["train", "val"]:
             y_path = os.path.join(self.data_dir, f"{self.split}_y", str(i).zfill(5) + ".npy")
-            y = torch.from_numpy(np.load(y_path))
+            y = torch.from_numpy(np.load(y_path)).long()
             
             return X, y, subject_idx
         else:
             return X, subject_idx
-        
+
+    def preprocess_data(self, X):
+        X = self.bandpass_filter(X)
+        X = self.normalize(X)
+        X = self.baseline_correction(X)
+        return X
+    
+    def butter_bandpass(self, lowcut, highcut, fs, order=4):
+        nyquist = 0.5 * fs
+        low = lowcut / nyquist
+        high = highcut / nyquist
+        b, a = butter(order, [low, high], btype='band')
+        return b, a
+
+    def bandpass_filter(self, X, lowcut=0.5, highcut=30.0, fs=200.0, order=4):
+        b, a = self.butter_bandpass(lowcut, highcut, fs, order=order)
+        y = lfilter(b, a, X.numpy(), axis=-1)
+        return torch.from_numpy(y).float()
+
+    def normalize(self, X):
+        mean = X.mean(dim=-1, keepdim=True)
+        std = X.std(dim=-1, keepdim=True)
+        return (X - mean) / std
+
+    def baseline_correction(self, X, baseline_window=(0, 50)):
+        baseline = X[:, baseline_window[0]:baseline_window[1]].mean(dim=-1, keepdim=True)
+        return X - baseline
+    
     @property
     def num_channels(self) -> int:
         return np.load(os.path.join(self.data_dir, f"{self.split}_X", "00000.npy")).shape[0]
